3807ecd8559b2f530b45ce67575bc94d178f63b0
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 13:52:18 +0100

    Update Cuadrado.js

--- Cuadrado.js
+++ Cuadrado.js
@@ -1,11 +1,6 @@
-// Que vale el cuadrado
 let lado = 40;
-
-// Calcular el área
 let area = lado * lado;
 
-// Calcular el perímetro
 let perimetro = 4 * lado;
-
 console.log("El área del cuadrado es:", area);
 console.log("El perímetro del cuadrado es:", perimetro);
